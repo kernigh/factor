487293d74df9ff2c4498ae391ca0f5488d1e8b5e
George Koehler <xkernigh@netscape.net>
2012-01-17 15:24:53 -0500

    Allow vm to compile again. (It broke after the last merge.)  * WARNING: I have no working images for the new vm,    so I cannot actually run Factor.

--- vm/os-openbsd-x86.64.hpp
+++ vm/os-openbsd-x86.64.hpp
@@ -9,4 +9,10 @@ static inline void uap_clear_fpu_status(void *uap) {}
 #define UAP_PROGRAM_COUNTER(ucontext) (((struct sigcontext *)ucontext)->sc_rip)
 #define UAP_SET_TOC_POINTER(uap, ptr) (void)0
 #define UAP_STACK_POINTER_TYPE long
+
+/* Must match the leaf-stack-frame-size, signal-handler-stack-frame-size,
+and stack-frame-size constants in basis/cpu/x86/64/unix/bootstrap.factor */
+static const unsigned LEAF_FRAME_SIZE = 16;
+static const unsigned SIGNAL_HANDLER_STACK_FRAME_SIZE = 160;
+static const unsigned JIT_FRAME_SIZE = 32;
 }
